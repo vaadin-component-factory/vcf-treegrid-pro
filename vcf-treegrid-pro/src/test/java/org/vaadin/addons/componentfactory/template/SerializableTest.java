c21cf4a25c9a8fabe23f869af97a41b00c53923e
Martin Lopez <mlopez@flowingcode.com>
2024-08-09 16:52:43 -0300

    feat(test): add basic unit tests

--- vcf-treegrid-pro/src/test/java/org/vaadin/addons/componentfactory/template/SerializableTest.java
+++ vcf-treegrid-pro/src/test/java/org/vaadin/addons/componentfactory/template/SerializableTest.java
@@ -1,30 +1,35 @@
 package org.vaadin.addons.componentfactory.template;
 
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.testutil.ClassesSerializableTest;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import java.util.stream.Stream;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class SerializableTest extends ClassesSerializableTest {
+import com.vaadin.flow.component.treegrid.TreeGridPro;
 
-    private static final UI FAKE_UI = new UI();
+public class SerializableTest {
 
-    @Override
-    protected void resetThreadLocals() {
-        super.resetThreadLocals();
-        UI.setCurrent(null);
-    }
+	  private void testSerializationOf(Object obj) throws IOException, ClassNotFoundException {
+	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+	      oos.writeObject(obj);
+	    }
+	    try (ObjectInputStream in =
+	        new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))) {
+	      obj.getClass().cast(in.readObject());
+	    }
+	  }
 
-    @Override
-    protected void setupThreadLocals() {
-        super.setupThreadLocals();
-        UI.setCurrent(FAKE_UI);
-    }
-
-    @Override
-    protected Stream<String> getExcludedPatterns() {
-        return Stream.concat(super.getExcludedPatterns(), Stream.of(
-                "com\\.vaadin\\..*"
-        ));
-    }
-}
+	  @Test
+	  public void testSerialization() throws ClassNotFoundException, IOException {
+	    try {
+	      testSerializationOf(new TreeGridPro<String>());
+	    } catch (Exception e) {
+	      Assert.fail("Problem while testing serialization: " + e.getMessage());
+	    }
+	  }
+	}
